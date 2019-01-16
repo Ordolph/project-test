8369ce2277c4c9a4afd3798cf2e1303fc7bdaa3f
Ordolph <austin.t.randolph@gmail.com>
2019-01-15 20:26:25 -0500

    Adjusted routing to test post

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,10 +5,14 @@ module.exports = function (app) {
     app.get("/", function (req, res) {
         res.render("index");
     });
-    app.get("/testGame", function(req, res){
+    app.get("/testGame", function (req, res) {
         res.render("testGame");
     });
     app.get("*", function (req, res) {
         res.render("404");
+    });
+    app.post("/" function (req, req) {
+        db.Player.create(req.body).then(result =>
+            res.json({ id: result.insertId }));
     });
 };
